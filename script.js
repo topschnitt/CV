7f76d9f4438c57bc55b2a4989f8e23ba8116fbbb
topschnitt <topschnitt@gmail.com>
2024-01-25 06:32:50 +0100

    minor changes

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Simulate a delay (2 seconds) for demonstration purposes
-    setTimeout(function() {
-        // Hide the loading screen
-        document.getElementById("loadingScreen").style.display = "none";
-        // Show the main content
-        document.getElementById("mainContent").style.display = "block";
-    }, 2000);
-});
