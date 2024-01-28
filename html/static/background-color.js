50537087a5b92c3320a441fb85d825c4ffd2d8a8
topschnitt <topschnitt@gmail.com>
2024-01-28 17:51:36 +0100

    background edit

---
+++ html/static/background-color.js
@@ -0,0 +1,26 @@
+<script>
+function load_section(id) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "section.html", false);
+    xhttp.send();
+    var sectionHtml = xhttp.responseText;
+    document.getElementById(id).innerHTML = sectionHtml;
+}
+
+function load_background_css(id, bgColor1, bgColor2, fontColor = "#000") {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "static/background.css", false);
+    xhttp.send();
+    var backgroundCss = xhttp.responseText;
+    var className = "background-" + id;
+    backgroundCss = backgroundCss.replaceAll("animated-container", className);
+    backgroundCss = backgroundCss.replace("#ffffff", bgColor1);
+    backgroundCss = backgroundCss.replace("#419db9", bgColor2);
+    backgroundCss = backgroundCss.replace("#000", fontColor);
+    const style = document.createElement('style');
+    style.innerHTML = backgroundCss;
+    document.head.appendChild(style);
+    document.getElementById(id).classList.add(className);
+}
+
+</script>
