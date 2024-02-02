49ae964abcc91d3ab530b5b039e7592658bb94a1
topschnitt <topschnitt@gmail.com>
2024-02-02 03:27:00 +0100

    2 sections

--- html/scripts/scripts.js
+++ html/scripts/scripts.js
@@ -1,7 +1,7 @@
 
 function load_section(id) {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "sections/section.html", false);
+    xhttp.open("GET", "sections/"+ id + ".html", false);
     xhttp.send();
     var sectionHtml = xhttp.responseText;
     document.getElementById(id).innerHTML = sectionHtml;
@@ -23,3 +23,28 @@ function load_background_css(id, bgColor1, bgColor2, fontColor = "#000") {
     document.getElementById(id).classList.add(className);
 }
 
+  // Function to add animation dynamically
+  function addAnimation(element) {
+    element.style.animation = 'slowly-appear 1s ease-in-out forwards';
+  }
+
+  // Wait for the DOM to be ready
+  document.addEventListener('DOMContentLoaded', function() {
+    // Select the element
+    var uniqueContainer = document.querySelector('.unique-container');
+
+    // Add animation when the element is in the viewport
+    window.addEventListener('scroll', function() {
+      var boundingBox = uniqueContainer.getBoundingClientRect();
+      var isInViewport = (
+        boundingBox.top >= 0 &&
+        boundingBox.left >= 0 &&
+        boundingBox.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        boundingBox.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+
+      if (isInViewport) {
+        addAnimation(uniqueContainer);
+      }
+    });
+  });
